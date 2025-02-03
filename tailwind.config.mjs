c3ad2178a54d3ab98e43a6459ffcc25fc3152f07
CodingCookiee <rzqutubawan@gmail.com>
2025-02-03 15:12:13 +0500

    commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,11 +8,33 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        generalsans: ["General Sans", "sans-serif"],
+        'sf-light': ['SF Pro Text Light', 'sans-serif'],
+        'sf-regular': ['SF Pro Text Regular', 'sans-serif'],
+        'sf-medium': ['SF Pro Text Medium', 'sans-serif'],
+        'sf-semibold': ['SF Pro Text Semibold', 'sans-serif'],
+        'sf-bold': ['SF Pro Text Bold', 'sans-serif'],
+        'sf-heavy': ['SF Pro Text Heavy', 'sans-serif'],
       },
+      colors: {
+    
+        light: {
+          primary: "#FFFFFF",
+          secondary: "#F0F2FA",
+          text: "#1A1A1A",
+          accent: "#5724ff",
+        },
+        dark: {
+          primary: "#0E0E10",
+          secondary: "#1C1C21",
+          text: "#FFFFFF",
+          accent: "#edff66",
+        },
+      },
+    
     },
   },
+  
   plugins: [],
 };
