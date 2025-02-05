3c9e9caae1f28ecc8bbc9abb75cb450368bdca38
CodingCookiee <rzqutubawan@gmail.com>
2025-02-05 19:15:34 +0500

    update

---
+++ src/app/api/signup/route.js
@@ -0,0 +1,21 @@
+import { signUp } from '@/services/controllers/auth.controller'
+import { rateLimit } from '@/services/middleware/rateLimit.middleware'
+import { NextResponse } from 'next/server'
+
+export async function POST(req) {
+  try {
+    await rateLimit(req)
+    const data = await req.json()
+    const user = await signUp(data)
+    
+    return NextResponse.json({ 
+      success: true,
+      user
+    })
+  } catch (error) {
+    return NextResponse.json({ 
+      success: false,
+      message: error.message 
+    }, { status: error.status || 500 })
+  }
+}
